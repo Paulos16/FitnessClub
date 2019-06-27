038356ce182b7bca27846ce5461299ac0c744412
Paulos16 <pawel.san16@gmail.com>
2019-06-27 11:07:09 +0200

    Added data annotations. Preparing models for first migration.

--- FitnessClub/Models/Customer.cs
+++ FitnessClub/Models/Customer.cs
@@ -1,12 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FitnessClub.Models
 {
     public class Customer
     {
+        [Key]
         public int IdCustomer { get; set; }
+        [Required]
+        [MaxLength(120)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(120)]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
         
         public int IdMembershipPlan { get; set; }
--- FitnessClub/Models/MembershipPlan.cs
+++ FitnessClub/Models/MembershipPlan.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FitnessClub.Models
 {
     public class MembershipPlan
     {
+        [Key]
         public int IdMembershipPlan { get; set; }
+        [Required]
+        [MaxLength(120)]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(120)]
         public string Description { get; set; }
         
         public List<Customer> Customers { get; set; }
